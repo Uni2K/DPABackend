f06f350afeb3d18852d4137d9cbabbe01adc8275
jonabecher <jonajbecher@gmail.com>
2020-02-01 20:48:57 +0100

    Feed doing

--- src/helpers/PoolBase.tsx
+++ src/helpers/PoolBase.tsx
@@ -1,12 +1,12 @@
-import {contentPoolModel} from "../models/ContentPool";
-import {feedPoolModel} from "../models/FeedPool";
+import {contentModel} from "../models/Content";
+import {feedModel} from "../models/FeedPool";
 import {userModel} from "../models/User";
 
-export class PoolItemBase{
+export class PoolBase{
 
     async createPoolItem(userID:Object, contentID:Object, contentType:String, priority:Number){
 
-        const content = new contentPoolModel({
+        const content = new contentModel({
             user : userID,
             content: contentID,
             contentType: contentType,
@@ -18,7 +18,7 @@ export class PoolItemBase{
 
     async createFeedItem(poolItem, index:number){
 
-        const content = new feedPoolModel({
+        const content = new feedModel({
             user : poolItem.user,
             content: poolItem.content,
             contentType: poolItem.contentType,
@@ -30,27 +30,34 @@ export class PoolItemBase{
         await poolItem.remove();
     }
 
+    async pollToPool(userID, pollID, topics){
+        let users = await userModel
+            .find({user: userID})
+            .sort({index:-1})
+            .select("_id");
+    }
+
     async changePoolItemPriority(contentID, priority:number, userID?){
         if(userID){
-            contentPoolModel.findOneAndUpdate({content: contentID, user: userID}, {priority: priority}).exec();
+            contentModel.findOneAndUpdate({content: contentID, user: userID}, {priority: priority}).exec();
         }
         else{
-            contentPoolModel.updateMany({content: contentID}, {priority: priority}).exec();
+            contentModel.updateMany({content: contentID}, {priority: priority}).exec();
         }
 
     }
 
     async getItemsForFeed(userID, amount:number){
 
-        //Items mit der höchsten Priorität und dem neusten Erstellungsdatum werden zuerst gesendet. Priorität > Erstellungsdatum
-        let feedItems = await contentPoolModel
+        // Items with the highest priority and the latest creation date are sent first. Priority > Creation Date
+        let feedItems = await contentModel
             .find({user: userID})
             .sort({priority:-1})
             .sort({"created_at": -1})
             .limit(amount)
             .exec();
 
-        let index = await feedPoolModel
+        let index = await feedModel
             .findOne({user: userID})
             .sort({index:-1})
             .select("index -_id");
@@ -74,7 +81,7 @@ export class PoolItemBase{
             if(i%2 === 0){
                 userCount = 1;
             }
-            const itemCreator = new PoolItemBase()
+            const itemCreator = new PoolBase()
             await itemCreator.createPoolItem(user[userCount],user[userCount],"cba", i)
         }
 
