5f6d984e2a8284e009c47a65eef0d9532e4110a9
jonabecher <jonajbecher@gmail.com>
2020-04-03 20:14:37 +0200

    .gitignore is now working

--- dist/db/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-});
-mongoose.set("useFindAndModify", false);
-//# sourceMappingURL=db.js.map
